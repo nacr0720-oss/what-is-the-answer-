6bb793e3fff43608d7070a1c7f3c9c00b8de2801
nacr0720-oss <nacr0720@gmail.com>
2025-10-26 11:35:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def load_questions(path: str = "questions.json") -> dict:
 questions = load_questions()
 
 # Layout: input grande e botão de limpar pequeno
-col_input, col_pass col_button = st.columns([2, 1, 1], vertical_alignment=['top', 'bottom','top'])
+col_input, col_pass, col_button = st.columns([2, 1, 1], vertical_alignment=['top', 'bottom','top'])
 with col_input:
     search = st.text_input("", placeholder="search", key="thylacocephalo")
     # mantemos o valor original (sem lower) para exibir, mas usaremos lower() ao comparar
@@ -50,6 +50,7 @@ else:
             st.write(str(answer))
     if not found:
         st.warning(f'Sorry, \"{search}\" was not found.')
+
 
 
 
