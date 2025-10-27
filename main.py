2aeca3271238e1405a504f05a1306d5ada7e2ea5
nacr0720-oss <nacr0720@gmail.com>
2025-10-27 18:25:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -55,13 +55,16 @@ else:
             sleep(0.5)
             cancel_text()
         elif search == 'B obctme':
+            found = True
             st.write('https://youtu.be/_koeuijFOEU')
         elif search == 'Which sequel to Hollow Knight features a new playable character named Hornet?':
-            st.write('yes... song not a spectogram... https://www.mediafire.com/file/6aecadesj7fw5x0/end.mp3/file')
+            found = True
+            st.write('yes... song not a spectogram... https://www.mediafire.com/file/89f2x7nd29srjg5/end+(1).mp3/file')
     if not found:
         st.warning(f'Sorry, \"{search}\" was not found.')
         sleep(60)
         st.write('please look too end')
+
 
 
 
